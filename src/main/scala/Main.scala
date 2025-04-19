d2ab10abb0ceec23672bee783ef328c2ec2b90cf
picimaci <fanni.medvey@gmail.com>
2025-04-19 17:28:13 +0200

    Combine service calls in Main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,12 @@
+import service.{ AlertTermMatchService, ApiService, ConfigService }
+import cats.syntax.traverse._
+
 object Main extends App {
-  println("Hello, World!")
+  for {
+    appConf         <- ConfigService.loadAppConfig
+    queryTerms      <- ApiService.getQueryTerms(appConf.queryTermUrl, appConf.apiKey)
+    alertApiIterator = (1 to appConf.numberOfAlertsFetch).toList
+    alertsList      <- alertApiIterator.traverse(_ => ApiService.getAlerts(appConf.alertUrl, appConf.apiKey))
+    matchResultsList = alertsList.map(alerts => AlertTermMatchService.findMatchingTermsForAlerts(queryTerms, alerts))
+  } yield {}
 }
