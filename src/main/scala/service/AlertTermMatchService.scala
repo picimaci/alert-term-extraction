5b70a8111998c47ab4b317afa9a09765246ae7c2
picimaci <fanni.medvey@gmail.com>
2025-04-19 17:58:29 +0200

    Add comments to services

--- src/main/scala/service/AlertTermMatchService.scala
+++ src/main/scala/service/AlertTermMatchService.scala
@@ -4,8 +4,20 @@ import model.{ Alert, AlertTermMatchResult, Content, QueryTerm, QueryTermId }
 
 object AlertTermMatchService {
 
+  // Convert string to lower case and split by whitespace characters
   private def lowerCaseSplitText(text: String): Array[String] = text.toLowerCase.split("\\s+")
 
+  /*
+    Create a normalized list of words for both the content and the query term
+      -> normalized as in lower case and split by whitespaces, see #lowerCaseSplitText
+
+    If keepOrder on the query term is true
+      - the content wordlist must contain the term wordlist in the exact order defined on the term
+    If keepOrder on the query term is false
+      - the content wordlist must contain all the elements in the term wordlist, the order is irrelevant
+
+    Return whether content contains query term
+   */
   def contentContainsTerm(content: Content, term: QueryTerm): Boolean = {
     val contentParts = lowerCaseSplitText(content.text)
     val termParts    = lowerCaseSplitText(term.text)
@@ -16,12 +28,27 @@ object AlertTermMatchService {
     }
   }
 
+  /*
+    Only check terms that are in the same language as the content
+    Filter query terms using #contentContainsTerm to find terms included in the content
+
+    Returns ids of query terms that the content contained
+   */
   def findContainedTermIds(content: Content, terms: Seq[QueryTerm]): Seq[QueryTermId] =
     terms
       .filter(_.language == content.language)
       .filter(term => contentContainsTerm(content, term))
       .map(_.id)
 
+  /*
+    Only check matches in contents that have the content type 'text'
+    Get the ids of the query terms that are contained in the alerts' contents by calling #findContainedTermIds
+    Convert term id list to set in order to remove duplicates
+      -> happens when a query term is contained in multiple contents of an alert
+    Remove results where no matching query term was found
+
+    Returns match results, which include an alert id and the query term ids that were included in the alert's contents
+   */
   def findMatchingTermsForAlerts(terms: Seq[QueryTerm], alerts: Seq[Alert]): Seq[AlertTermMatchResult] =
     alerts
       .map { alert =>
--- src/main/scala/service/FileService.scala
+++ src/main/scala/service/FileService.scala
@@ -11,11 +11,18 @@ import util.ErrorHandler._
 object FileService {
   private implicit val logger: Logger = Logger(getClass.getName)
 
+  /*
+    In order to have a clean slate on each run remove results folder
+    if exists and create new one for saving the results
+   */
   private def resetResultFolder(resultFolderName: String): File = {
     rm(file"$resultFolderName")
     mkdir(file"$resultFolderName")
   }
 
+  /*
+    Reset results folder and save match results in json files per fetched alert list
+   */
   def saveResults(resultFolderName: String, resultsList: Seq[Seq[AlertTermMatchResult]]): Either[String, Unit] = {
     val dir = resetResultFolder(resultFolderName)
     logger.info(s"Directory $dir removed and recreated successfully")
